3522ba5d7c15430b074eddd6e893d87811866e3d
Emily Zhao <tong.zhao.92@gmail.com>
2018-01-23 19:46:26 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -71,8 +71,9 @@ class MusicLibraryController
     input = gets.strip.to_i
     if (1..Song.all.length).include?(input)
       song = Song.all.sort{ |a, b| a.name <=> b.name }[input - 1]
+      puts "Playing #{song.name} by #{song.artist.name} in the directory."
     end
-    puts "Playing #{song.name} by #{song.artist.name} in the directory."
+
   end
 
 end
