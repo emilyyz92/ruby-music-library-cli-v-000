b8e321865ba061f5287fbd6071d2d820fac9b0f6
Emily Zhao <tong.zhao.92@gmail.com>
2018-01-23 20:41:10 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -20,17 +20,18 @@ class MusicLibraryController
 
     input = gets.strip
 
-    if input == "list songs"
+    case input
+    when "list songs"
       self.list_songs
-    elsif input == "list artists"
+    when "list artists"
       self.list_artists
-    elsif input == "list genres"
+    when "list genres"
       self.list_genres
-    elsif input == "list artist"
+    when "list artist"
       self.list_songs_by_artist
-    elsif input == "list genre"
+    when "list genre"
       self.list_songs_by_genre
-    elsif input == "play song"
+    when "play song"
       self.play_song
     end
   end
